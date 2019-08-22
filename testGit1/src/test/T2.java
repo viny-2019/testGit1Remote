41425de4b1c00074905a7a7a19fd9757efe304ef
viny <forsunmoon@163.com>
2019-08-22 17:03:53 +0800

    提交viny.demo.Demo1.java    test.T2.java    test.T3.java

---
+++ testGit1/src/test/T2.java
@@ -0,0 +1,9 @@
+package test;
+
+public class T2 {
+
+	public static void main(String[] args) {
+		System.out.println("test T2");
+	}
+
+}
---
+++ testGit1/src/viny/demo/Demo1.java
@@ -0,0 +1,9 @@
+package viny.demo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("test Demo1");
+	}
+
+}
